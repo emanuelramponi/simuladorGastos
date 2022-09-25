b4e5a5099db661f2f4f60ca3111ab92c8f6191cf
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-09-25 13:50:27 -0300

    CORRIGIENDO ERRORES II

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-let opcionesCategorias = [] // creo un array para almacenar los objetos de las opciones
-let arrayCategoria = ["Alimentacion", "Cuentas y Servicios", "Hogar", "Transporte", "Ropa", "Salud e Higiene", "Otros"] // CATEGORIAS PREDEFINIDAS
+let opcionesCategorias = []                                                 // CREO UN ARRAY PARA ALMACENAR LOS OBJETOS DE LAS OPCIONES                                                        
+let arrayCategoria = [
+    "Alimentacion",                                       // CATEGORIAS PREDEFINIDAS
+    "Cuentas y Servicios", 
+    "Hogar", 
+    "Transporte", 
+    "Ropa", 
+    "Salud e Higiene", 
+    "Otros"
+    ]                                                    
 const selectTag = document.getElementById('lista')
 const descripcionInput = document.getElementById('descripcion')
 const valorInput = document.getElementById('valor')
 const divCategorias = document.getElementsByClassName('divCategorias')
 const container2 = document.getElementById('btnAdd') 
-const gastosLocalstrg = JSON.parse(localStorage.getItem('gastos')) || [] // operadores avanzados, si hay algun elemento dentro del local storage que se guarde sino que quede vacio el array
+const gastosLocal = JSON.parse(localStorage.getItem('gastos')) || []    // OPERADORES AVANZADOS, SI HAY ALGUN ELEMENTO DENTRO DEL LOCAL STORAGE, HACEMOS QUE SE GUARDE, SINO QUE QUEDE VACIO EL ARRAY
 
 
-class Categorias { // CREAMOS UN OBJETO CON UN ID Y CATEGORIA PARA CADA UNA DE LAS MISMAS
+console.log(gastosLocal);
+
+class Categorias {                                                          // CREAMOS UN OBJETO CON UN ID Y CATEGORIA PARA CADA UNA DE LAS MISMAS
     constructor(id, categoria,descripcion,valor) {
         this.id = id
         this.categoria = categoria
@@ -16,7 +26,7 @@ class Categorias { // CREAMOS UN OBJETO CON UN ID Y CATEGORIA PARA CADA UNA DE L
         this.valor = valor
     }
 
-    desplegarProductos() { // ponemos el codigo html para cada categoria 
+    desplegarProductos() {                                                  // PONEMOS EL CODIGO HTML EN CADA CATEGORIA
         const card = `
         <div class="accordion-item">
             <h2 class="accordion-header" id="categoria${this.id}h2">
@@ -31,25 +41,24 @@ class Categorias { // CREAMOS UN OBJETO CON UN ID Y CATEGORIA PARA CADA UNA DE L
         </div>
         `
         const container = document.getElementById('accordion')
-        container.innerHTML += card // LE AGREGO EL + PARA IR SUMANDOLE LAS CARDS
+        container.innerHTML += card                                         // LE AGREGO EL + PARA IR SUMANDOLE LAS CARDS
     }
 }
 
-for (let i = 0; i < arrayCategoria.length; i++) { // ASIGNAMOS EL ARRAY DE LAS CATEGORIAS AL OTRO ARRAY CON LOS OBJETOS
+for (let i = 0; i < arrayCategoria.length; i++) {                           // ASIGNAMOS EL ARRAY DE LAS CATEGORIAS AL OTRO ARRAY CON LOS OBJETOS
     console.log(arrayCategoria[i])
     opcionesCategorias.push(new Categorias(i + 1, arrayCategoria[i]))
 }
 
-opcionesCategorias.forEach(e => { // DESPLEGAMOS LAS OPCIONES 
+opcionesCategorias.forEach(e => {                                           // DESPLEGAMOS LAS OPCIONES 
     e.desplegarProductos()
 }) 
 
 
-/* ========================================================================================================================= */
 
-//EVENTOS
+//==========================================================EVENTOS====================================================================================
 
-opcionesCategorias.forEach(opcionesCategorias => { // AGREGAMOS LAS OPCIONES DEL ARRAY AL DOM
+opcionesCategorias.forEach(opcionesCategorias => {                          // AGREGAMOS LAS OPCIONES DEL ARRAY AL DOM
     const option = document.createElement('option')
     option.innerText = `${opcionesCategorias.categoria}`
     selectTag.append(option)
@@ -62,7 +71,6 @@ const containerDivBtn = document.createElement('p')
 const formulario = document.getElementById('form_gastos')
 formulario.onsubmit = (e) => {
     e.preventDefault()
-    let condicionNull = 0
     let gasto = {
         id : parseInt(selectTag.selectedIndex + 1),
         categoria: selectTag.value,
@@ -71,39 +79,61 @@ formulario.onsubmit = (e) => {
     }
 
     const {id,categoria,descripcion,valor} = gasto
-/*     const object = {}
-    obj['descripcion'] =  */
-/*    
-    } */
 
 
-     Array.from(e.target.children).forEach(children =>{
-        /* input === null || condicionNull++ */
-        Array.from(children.children).forEach(input =>{
-            console.log(input.value);
-        })
-        
-     })
-    
-    console.log(e);
-    if(condicionNull>0){
-        gastosLocalstrg.push(new Categorias(id,categoria,descripcion,valor))
-        localStorage.setItem('gastos', JSON.stringify(gastosLocalstrg))
-/*      */   
-    }else{
+    if(valorNotNull(descripcion,valor)){
         containerDivBtn.innerText = "Ingreso algun dato invalido"
         container2.append(containerDivBtn)
-    }
+/*      */   
+    }else{
+        gastosLocal.push(gasto)
+        localStorage.setItem('gastos', JSON.stringify(gastosLocal))
+        agregarGastos(gastosLocal)
+
+    }      
+    formulario.reset()
 }
 
-/* agregarGastos(localStorage.getItem(`gastos`))
- */
+
+function valorNotNull(descripcion,valor){
+
+    let invalidNumber = valor || true  // si valor es null o vacio o igual a 0, arroja true
+    let invalidString = descripcion || true // idem arriba
+
+    return invalidNumber !== true && invalidNumber <= 0 ? invalidNumber = true : invalidString === true || invalidNumber === true? true : false
+    /* 
+    operadores, si invalidNumber es diferente de true y menor a 0, se convierte a true, 
+    si invalidNumber es estrictamente igual a true o invalidString es estrictamente igual a true,
+    retornar true si se cumple alguna de las condiciones, sino retornar false 
+    */
+}
 
 
+function agregarGastos(array){
+    containerDivBtn.remove()
+    
+    opcionesCategorias.forEach(elemento=>{
+        let container = document.getElementById(`categoria${elemento.id}`)
+        container.innerHTML =""
+    })
 
-/* function agregarGastos(gasto){
-        
-        containerDivBtn.remove()
+    array.forEach(elemento=>{
+        let div = document.createElement("div")
+        div.innerHTML = `   <div class="addGastos btn-group d-flex justify-content-between" role="group" aria-label="Basic checkbox toggle button group">
+                                <input type="checkbox" class="btn-check" id="btnCheck${elemento.id}" autocomplete="off">
+                                <label class="btn btn-outline-primary d-flex justify-content-between" for="btnCheck${elemento.id}">
+                                <div class="descripcionaccordion" >${elemento.descripcion}</div>
+                                <div class="valoraccordion">${elemento.valor}</div>  
+                                </label>
+                            </div>`
+        let container = document.getElementById(`categoria${elemento.id}`)
+        console.log(container)
+        container.appendChild(div)
+    })
+
+
+}
+/*         containerDivBtn.remove()
         console.log(gasto);
         const new_conversion = JSON.parse(gasto)
         gastosLocal.push(new_conversion)
@@ -122,7 +152,6 @@ formulario.onsubmit = (e) => {
         container2.innerHTML += card
 
         containerDivBtn.innerText = "Gasto agregado"
-        container2.append(containerDivBtn)
-    }    
+        container2.append(containerDivBtn) */
+       
 
-  */
